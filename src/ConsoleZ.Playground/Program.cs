0129b65b61bf4200083bbbd1682478dd23ce55ca
Guy Langston <guylangston@hotmail.com>
2019-01-31 09:15:24 +0000

    Misc

--- src/ConsoleZ.Playground/Program.cs
+++ src/ConsoleZ.Playground/Program.cs
@@ -16,30 +16,32 @@ namespace ConsoleZ.Playground
             var screen = DirectConsole.Singleton;
 
             var frameCount = 2000;
-            var frameDelayMs = 10;
+            Console.WriteLine(Benchmark(frameCount, screen));
+        }
+
+        private static string Benchmark(int frameCount, IBufferedAbsConsole<CHAR_INFO> screen)
+        {
             var r = new Random();
             var stop = new Stopwatch();
             stop.Start();
             for (int i = 0; i < frameCount; i++)
             {
-                //screen.Fill(new CHAR_INFO(' ', 0));
                 var x = r.Next(screen.Width);
                 var y = r.Next(screen.Height);
                 var a = r.Next(26);
+                var clr = r.Next(0, 255);
 
-                screen[x, y] = new CHAR_INFO(TestHelper.GetCharOffset('A', a), 0x3F);
+                screen[x, y] = new CHAR_INFO(TestHelper.GetCharOffset('A', a), (ushort)clr);
                 screen.Update();
 
                 if (i % 50 == 0)
                 {
                     System.Console.Title = i.ToString();
                 }
-                
-
-                //Thread.Sleep(frameDelayMs);
             }
+
             stop.Stop();
-            Console.WriteLine($"{frameCount} frames in {stop.Elapsed} at {frameCount / stop.Elapsed.TotalSeconds} FPS");
+            return $"{frameCount} frames in {stop.Elapsed} at {frameCount / stop.Elapsed.TotalSeconds} FPS";
         }
     }
 }
