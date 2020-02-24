95a5c3119c21b05bc197ce8265b72220c4d45961
Guy Langston <guylangston@gmail.com>
2020-02-24 11:46:36 +0000

    Misc

--- src/ConsoleZ/Win32/ConsoleInteropHelper.cs
+++ src/ConsoleZ/Win32/ConsoleInteropHelper.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ConsoleZ.Win32
 {
-    internal class ConsoleInteropHelper
+    public static class ConsoleInteropHelper
     {
         // https://pinvoke.net/default.aspx/kernel32/GetStdHandle.html
         const int STD_OUTPUT_HANDLE = -11;
@@ -11,23 +11,39 @@ namespace ConsoleZ.Win32
         public static IntPtr Get_STD_OUTPUT_HANDLE => ConsoleInterop.GetStdHandle(STD_OUTPUT_HANDLE);
         public static IntPtr Get_STD_INPUT_HANDLE => ConsoleInterop.GetStdHandle(STD_INPUT_HANDLE);
         
-        public static void EnableMouseSupport()
+        const uint ENABLE_MOUSE_INPUT = 0x0010,
+            ENABLE_QUICK_EDIT_MODE    = 0x0040,
+            ENABLE_EXTENDED_FLAGS     = 0x0080,
+            ENABLE_ECHO_INPUT         = 0x0004,
+            ENABLE_WINDOW_INPUT       = 0x0008; //more
+        
+        public static bool EnableMouseSupport()
         {
             // https://stackoverflow.com/questions/1944481/console-app-mouse-click-x-y-coordinate-detection-comparison
-            const uint ENABLE_MOUSE_INPUT = 0x0010,
-                ENABLE_QUICK_EDIT_MODE = 0x0040,
-                ENABLE_EXTENDED_FLAGS = 0x0080,
-                ENABLE_ECHO_INPUT = 0x0004,
-                ENABLE_WINDOW_INPUT = 0x0008; //more
-            
-            
             var inputHandle = Get_STD_INPUT_HANDLE;
             uint mode = 0;
-            ConsoleInterop.GetConsoleMode(inputHandle, out mode);
+            if (!ConsoleInterop.GetConsoleMode(inputHandle, out mode)) return false;
+            
             mode &= ~ENABLE_QUICK_EDIT_MODE; //disable
             //mode |= ENABLE_WINDOW_INPUT; //enable (if you want)
             mode |= ENABLE_MOUSE_INPUT; //enable
-            ConsoleInterop.SetConsoleMode(inputHandle, mode);
+            
+            if (!ConsoleInterop.SetConsoleMode(inputHandle, mode)) return false;
+
+            return true;
+        }
+
+        public static bool DisableQuickEdit()
+        {
+            var  inputHandle = Get_STD_INPUT_HANDLE;
+            uint mode        = 0;
+            if (!ConsoleInterop.GetConsoleMode(inputHandle, out mode)) return false;
+            
+            mode &= ~ENABLE_QUICK_EDIT_MODE; //disable
+            
+            if (!ConsoleInterop.SetConsoleMode(inputHandle, mode)) return false;
+
+            return true;
         }
     }
 }
