6caac28dde5524dd2b58cdb1e6a3f31a0920392b
Guy Langston <guylangston@gmail.com>
2020-04-18 10:24:58 +0100

    Upgraded, fixed deploy script

---
+++ src/__publish.ps1
@@ -0,0 +1,30 @@
+param(
+     [string]$ver
+    )
+
+ls C:\Projects\LocalNuGet\
+
+if ($ver -eq ""){
+    $ver = Read-Host -Prompt 'Version as "0.1.6"'
+}
+
+pushd
+#$ver = "0.1.6"
+
+cd ConsoleZ
+dotnet build -c Release
+dotnet pack -c Release "-p:PackageVersion=$ver"
+#ls .\bin\Release
+copy ".\bin\Release\ConsoleZ.$ver.nupkg"  "C:\Projects\LocalNuGet\ConsoleZ.$ver.nupkg"
+
+cd ..
+cd ConsoleZ.AspNetCore
+dotnet build -c Release
+dotnet pack -c Release "-p:PackageVersion=$ver"
+#ls .\bin\Release
+copy ".\bin\Release\ConsoleZ.AspNetCore.$ver.nupkg"  "C:\Projects\LocalNuGet\ConsoleZ.AspNetCore.$ver.nupkg"
+
+echo "C:\Projects\LocalNuGet\"
+ls C:\Projects\LocalNuGet\
+
+popd
