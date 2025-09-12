3b191e0c2d8742711bf46958746196693b1b934e
Guy Langston <guylangston@gmail.com>
2025-09-12 10:22:59 +0100

    misc: QOL overload

--- src/ConsoleZ.Core/TUI/RichWriterFluent.cs
+++ src/ConsoleZ.Core/TUI/RichWriterFluent.cs
@@ -29,6 +29,7 @@ public abstract class RichWriterFluent<TClr, TStyle, TSelf> : TextWriterFluentBa
     }
     public TSelf Set(TClr fg)                        { inner.Set(fg); return Self; }
     public TSelf Set(TClr fg, TClr bg)               { inner.Set(fg, bg); return Self; }
+    public TSelf Set(TextClr<TClr> txtClr)               { inner.Set(txtClr.Fg, txtClr.Bg); return Self; }
     public TSelf Set(TClr fg, TClr bg, TStyle style) { inner.Set(fg, bg, style); return Self; }
     public TSelf SetBack(TClr bg)                    { inner.Back = bg; return Self; }
     public TSelf SetStyle(TStyle style)                    { inner.Style = style; return Self; }
@@ -40,6 +41,13 @@ public abstract class RichWriterFluent<TClr, TStyle, TSelf> : TextWriterFluentBa
     public TSelf WriteIf(bool test, TClr fg, ReadOnlySpan<char> txt) { if (!test) return Self; Set(fg); inner.Write(txt); return Self; }
     public TSelf WriteIf(bool test, ReadOnlySpan<char> txt) { if (test) inner.Write(txt); return Self; }
 
-    public TSelf Write(TClr fg, ReadOnlySpan<char> txt)                        { inner.Set(fg); inner.Write(txt); return Self; }
+    public TSelf Write(TClr fg, ReadOnlySpan<char> txt)
+    {
+        inner.Push();
+        inner.Set(fg);
+        inner.Write(txt);
+        inner.Pop();
+        return Self;
+    }
 }
 
